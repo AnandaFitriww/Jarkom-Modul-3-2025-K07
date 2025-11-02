13f40413edbbcd58a69f5d960c632e2e3c6d476d
AnandaFitriww <anandawibowo411@gmail.com>
2025-11-02 22:44:59 +0700

    Update 9.sh

--- 9.sh
+++ 9.sh
@@ -22,19 +22,3 @@ curl http://isildur.K07.com:8002/api/airing
 # Uji Anarion
 lynx -dump http://anarion.K07.com:8003/api/airing
 curl http://anarion.K07.com:8003/api/airing
-
-
-
-
-
-
-
-# Palantir – Database Server (10.67.4.3)
-# Instal dan siapkan MariaDB
-printf "nameserver 10.67.5.2\noptions timeout:2 attempts:2\n" >/etc/resolv.conf
-apt update -o Acquire::ForceIPv4=true -y
-apt install -y mariadb-server mariadb-client
-
-# Buka akses jaringan
-sed -i 's/^\(\s*bind-address\s*=\s*\).*/\10.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-
